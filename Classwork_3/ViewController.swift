df0b3f833ddd26a7825f567169437387a2d18b31
Maria Slepneva <marsppk2003@gmail.com>
2023-10-04 14:45:03 +0300

    Initial Commit

---
+++ Classwork_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Classwork_3
+//
+//  Created by Maria Slepneva on 04.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
